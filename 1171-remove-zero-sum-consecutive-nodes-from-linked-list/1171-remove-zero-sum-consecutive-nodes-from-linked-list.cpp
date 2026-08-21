6cbee40bfabcf4b733696bcfa87ec8cab49698d5
Piyush 🎮💜 <piyush0748in@gmail.com>
2026-08-21 18:17:25 +0530

    Time: 1 ms (69.46%), Space: 16.1 MB (71.78%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        unordered_map<int, ListNode*> mp;
+
+        int sum = 0;
+        ListNode* temp = dummy;
+
+        // First pass
+        while (temp != NULL) {
+            sum += temp->val;
+            mp[sum] = temp;
+            temp = temp->next;
+        }
+
+        // Second pass
+        sum = 0;
+        temp = dummy;
+
+        while (temp != NULL) {
+            sum += temp->val;
+            temp->next = mp[sum]->next;
+            temp = temp->next;
+        }
+
+        return dummy->next;
+    }
+};
